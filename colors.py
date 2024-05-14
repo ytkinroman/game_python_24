cfb4668bda6cde2c71cf482290145ac39868883f
Ytkin Roman <mc3heisenberg@gmail.com>
2024-05-14 15:28:37 +0500

    Add colors.py

---
+++ colors.py
@@ -0,0 +1,9 @@
+class Colors:
+    """Класс для работы с цветами."""
+    color_red = (255, 0, 0)
+    color_green = (0, 255, 0)
+    color_blue = (0, 0, 255)
+    color_yellow = (255, 255, 0)
+    color_black = (0, 0, 0)
+    color_white = (255, 255, 255)
+    color_gray = (192, 192, 192)
