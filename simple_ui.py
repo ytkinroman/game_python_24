2d1764e959cc54b7a2d40ae800536e505065b53b
Ytkin Roman <mc3heisenberg@gmail.com>
2024-05-15 23:01:50 +0500

    Update simple_ui.py

--- simple_ui.py
+++ simple_ui.py
@@ -3,19 +3,23 @@ from game_settings import GameSettings
 
 
 class UI:
-    def __init__(self):
+    def __init__(self) -> None:
+        """Инициализация списка элементов пользовательского интерфейса."""
         self.elements = []
 
-    def add_element(self, element):
+    def add_element(self, element: pg.Surface) -> None:
+        """Добавление элемента в список элементов пользовательского интерфейса."""
         self.elements.append(element)
 
-    def draw(self, screen):
+    def draw(self, screen: pg.Surface) -> None:
+        """Отрисовка элементов пользовательского интерфейса на экране."""
         for element in self.elements:
             element.draw(screen)
 
 
 class Text:
-    def __init__(self, text: str, size: int, color, position) -> None:
+    def __init__(self, text: str, size: int, color: tuple[int, int, int], position: tuple[int, int, int]) -> None:
+        """Инициализация текстового элемента пользовательского интерфейса."""
         self.text = text
         self.size = size
         self.position = position
@@ -25,10 +29,11 @@ class Text:
         self.rect = self.surface.get_rect(center=self.position)
 
     def draw(self, screen: pg.Surface) -> None:
+        """Отрисовка текстового элемента на экране."""
         screen.blit(self.surface, self.rect)
 
     def set_text(self, new_text):
+        """Изменение текста текстового элемента."""
         self.text = new_text
         self.surface = self.font.render(self.text, True, self.color)
         self.rect = self.surface.get_rect(center=self.position)
-
