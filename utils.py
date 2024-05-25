d69c4fbd1a4cd3c7643476b8f6210b9a829de917
Ytkin Roman <mc3heisenberg@gmail.com>
2024-05-25 16:29:01 +0500

    Upd utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
-import pygame as pg
+class GameSettings:
+    """Класс для хранения настроек игры."""
+    SCREEN_WIDTH, SCREEN_HEIGHT = 1024, 768
+    SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+    SCREEN_TITLE = "PyGame | PyGame PyGame | Name Name Name Name Name Name"
+    SCREEN_ICON = "images/screen_game_logo.png"
+
+    GAME_FPS_MAX = 60
+    GAME_BASE_FONT = "fonts/thin_pixel-7.ttf"
+    GAME_BASE_FONT_SIZE = 40
 
 
-class BackgroundSprite(pg.sprite.Sprite):
-    def __init__(self, image_path):
-        super().__init__()
-        self.image = pg.image.load(image_path).convert()
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (0, 0)  # РАСПОЛОЖЕНИЕ В ВЕРХНЕМ ЛЕВОМ УГЛУ
+class Colors:
+    """Класс для хранения часто используемых цветов."""
+    COLOR_RED = (255, 0, 0)
+    COLOR_GREEN = (0, 255, 0)
+    COLOR_BLUE = (0, 0, 255)
+    COLOR_YELLOW = (255, 255, 0)
+    COLOR_BLACK = (0, 0, 0)
+    COLOR_WHITE = (255, 255, 255)
+    COLOR_GRAY = (192, 192, 192)
