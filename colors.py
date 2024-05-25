b5b322bc5eca4d9bb9560a457eb1280fe27a72a2
Ytkin Roman <mc3heisenberg@gmail.com>
2024-05-25 16:16:36 +0500

    Upd colors.py

--- colors.py
+++ colors.py
@@ -1,9 +1,9 @@
 class Colors:
     """Класс для работы с цветами."""
-    color_red = (255, 0, 0)
-    color_green = (0, 255, 0)
-    color_blue = (0, 0, 255)
-    color_yellow = (255, 255, 0)
-    color_black = (0, 0, 0)
-    color_white = (255, 255, 255)
-    color_gray = (192, 192, 192)
+    COLOR_RED = (255, 0, 0)
+    COLOR_GREEN = (0, 255, 0)
+    COLOR_BLUE = (0, 0, 255)
+    COLOR_YELLOW = (255, 255, 0)
+    COLOR_BLACK = (0, 0, 0)
+    COLOR_WHITE = (255, 255, 255)
+    COLOR_GRAY = (192, 192, 192)
