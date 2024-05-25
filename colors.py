ae0a57e35a68c990ae8061740e17746aea7b5593
Ytkin Roman <mc3heisenberg@gmail.com>
2024-05-25 18:14:28 +0500

    Update utils.py

--- colors.py
+++
@@ -1,9 +0,0 @@
-class Colors:
-    """Класс для работы с цветами."""
-    COLOR_RED = (255, 0, 0)
-    COLOR_GREEN = (0, 255, 0)
-    COLOR_BLUE = (0, 0, 255)
-    COLOR_YELLOW = (255, 255, 0)
-    COLOR_BLACK = (0, 0, 0)
-    COLOR_WHITE = (255, 255, 255)
-    COLOR_GRAY = (192, 192, 192)
