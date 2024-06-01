cf74833e9a8774354074be541cf43329b80d9104
Ytkin Roman <mc3heisenberg@gmail.com>
2024-06-01 13:21:48 +0500

    Update scene.py

--- scene_module/scene.py
+++ scene_module/scene.py
@@ -3,16 +3,15 @@ import pygame as pg
 
 class Scene:
     """Базовый класс для всех сцен в игре."""
-    def __init__(self) -> None:
-        """Инициализация сцены."""
-        pass
+    def __init__(self, game) -> None:
+        self._game = game
 
-    def handle_events(self, events: list[pg.event.Event]) -> None:
+    def handle_events(self, event: pg.event.Event) -> None:
         """Обработка нажатий."""
         pass
 
     def update(self, delta_time: float) -> None:
-        """Обновление состояния сцены."""
+        """Обновление сцены."""
         pass
 
     def render(self, screen: pg.Surface) -> None:
