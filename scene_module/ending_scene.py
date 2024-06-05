79de481ae6e92b1117139445d6eab25b1cfd4553
Ytkin Roman <mc3heisenberg@gmail.com>
2024-06-05 14:58:26 +0500

    Upd ending_scene.py, ending_ui.py

--- scene_module/ending_scene.py
+++ scene_module/ending_scene.py
@@ -6,7 +6,18 @@ from ui_module.ending_ui import UIEnding
 class EndingScene(Scene):
     def __init__(self, game) -> None:
         super().__init__(game)
-        self._final_scene_ui = UIEnding(self._game)
+        self._game = game
+        self._ending_good = None
+        self._final_scene_ui = UIEnding(self._game, self._ending_good)
+
+    def set_good_ending(self):
+        self._ending_good = True
+
+    def set_bad_ending(self):
+        self._ending_good = False
+
+    def reset(self) -> None:
+        self._ending_good = None
 
     def render(self, screen: pg.Surface) -> None:
         self._final_scene_ui.draw(screen)
@@ -16,5 +27,4 @@ class EndingScene(Scene):
             if event.key == pg.K_ESCAPE:
                 self._game.stop()
             elif event.key == pg.K_SPACE:
-                pass
-                # self._game.change_scene("game")
+                self._game.new_game()
